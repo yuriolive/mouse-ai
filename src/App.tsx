4acf2f7fbaadf4a396cb6797c06b48c1320dac8c
Yuri Olive <yuriso1994@gmail.com>
2024-05-01 18:17:38 -0300

    feat: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Input } from "@nextui-org/react";
+
+
 function App() {
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-    </div>
+      <div>
+          <h1>Mouse AI</h1>
+          <div className="flex w-full flex-wrap md:flex-nowrap gap-4">
+              <Input type="email" label="Email"/>
+              <Input type="email" label="Email" placeholder="Enter your email"/>
+          </div>
+      </div>
   );
 }
 
